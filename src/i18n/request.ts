280d1b97d4dd0f75eca3cc329e546f08dd418a9b
maagyei04 <maagyei04@gmail.com>
2024-11-15 14:11:49 +0000

    request config fixed

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -6,7 +6,14 @@ export default getRequestConfig(({ locale }) => {
   // Validate that the incoming `locale` parameter is valid
   if (!routing.locales.includes(locale as any)) notFound();
 
+  // The `messages` property should be an object containing the translation messages
+  // for the specified locale, or `undefined` if you don't have translations for that locale
+  const messages = {
+    // ... your translation messages for the `locale` ...
+  };
+
+  // Return the configuration object with the `messages` property
   return {
-    messages: null
+    messages,
   };
 });
